e916e86d18931e56961a247d78f63eafaacfe92b
peter <peter@retep.org>
2021-08-27 18:03:16 +0100

    Remove generator dependency

--- tools/hugo/hugo.go
+++ tools/hugo/hugo.go
@@ -2,7 +2,6 @@ package hugo
 
 import (
   "github.com/peter-mount/documentation/tools/util"
-  "github.com/peter-mount/go-kernel"
   "log"
   "os/exec"
 )
@@ -13,10 +12,6 @@ type Hugo struct {
 
 func (h *Hugo) Name() string {
   return "hugo"
-}
-
-func (h *Hugo) Init(k *kernel.Kernel) error {
-  return k.DependsOn(&Generator{})
 }
 
 func (h *Hugo) Run() error {
