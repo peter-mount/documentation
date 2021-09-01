f338e2a04292b51635760c7d99f03b9817d5eb8f
peter <peter@retep.org>
2021-09-01 15:11:21 +0100

    When using -s bind to all ports so that it can be exposed via docker run -p

--- tools/hugo/hugo.go
+++ tools/hugo/hugo.go
@@ -1,26 +1,39 @@
 package hugo
 
 import (
+  "flag"
   "github.com/peter-mount/documentation/tools/util"
+  "github.com/peter-mount/go-kernel"
   "log"
   "os/exec"
 )
 
 // Hugo runs hugo
 type Hugo struct {
+  server *bool // true to run Hugo in server mode
 }
 
 func (h *Hugo) Name() string {
   return "hugo"
 }
 
+func (h *Hugo) Init(_ *kernel.Kernel) error {
+  h.server = flag.Bool("s", false, "Run hugo in server mode")
+  return nil
+}
+
 func (h *Hugo) Run() error {
   log.Println("Running hugo")
+
+  var args []string
+  if *h.server {
+    args = append(args, "server", "--bind=0.0.0.0", "--port=1313")
+  }
 
   stdout := &util.LogStream{}
   defer stdout.Close()
 
-  cmd := exec.Command("hugo")
+  cmd := exec.Command("hugo", args...)
   cmd.Stdout = stdout
   cmd.Stderr = stdout
 
