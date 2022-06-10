0e79e542cb1833bfabfb75add7b8df411e45f725
peter <peter@retep.org>
2022-06-10 20:33:29 +0100

    Fix vazir-font

--- static/static/cdn/font-transform.sh
+++ static/static/cdn/font-transform.sh
@@ -3,14 +3,15 @@
 newPath=$1
 fontCss=font.css
 
+src="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v27.0.1/dist/"
 FILES=$(grep "url(" ${fontCss}|cut -f2 -d'('|cut -f1 -d')')
 for file in $FILES
 do
-  fileName=$(basename $file)
+  fileName=$(basename $file | sed "s/'//g")
   dirName=$(dirname $file)
 
   echo "$file -> $fileName"
-  wget --quiet -O $fileName $file
+  wget --quiet -O $fileName "${src}$(echo -n $file | sed "s/'//g")"
 
-  sed -i "s|${dirName}|${newPath}|g" ${fontCss}
+  #sed -i "s|${dirName}|${newPath}|g" ${fontCss}
 done
