29c258ba60caf9b8037a7e4875ca5336cf769f10
peter <peter@retep.org>
2023-02-22 19:15:45 +0000

    Committing latex code which isn't yet ready

--- tools/latex/css/css.go
+++ tools/latex/css/css.go
@@ -1,84 +1,84 @@
 package css
 
 import (
-	"golang.org/x/net/html"
-	"io"
-	"log"
-	"os"
+  "golang.org/x/net/html"
+  "io"
+  "log"
+  "os"
 )
 
 // Css represents a simple CSS stylesheet for rendering latex
 type Css struct {
-	Styles *Styles `kernel:"config,css"`
+  Styles *Styles `kernel:"config,css"`
 }
 
 type Styles struct {
-	Styles []*Style `yaml:"styles"`
+  Styles []*Style `yaml:"styles"`
 }
 
 type Style struct {
-	Rule     *Rule             `yaml:"-"`        // The compiled rule
-	RuleSrc  string            `yaml:"rule"`     // Rule e.g. td:not(:last-child)
-	Css      map[string]string `yaml:"css"`      // CSS to apply
-	Children []*Style          `yaml:"children"` // Child rules, processed only if this one matches
+  Rule     *Rule             `yaml:"-"`        // The compiled rule
+  RuleSrc  string            `yaml:"rule"`     // Rule e.g. td:not(:last-child)
+  Css      map[string]string `yaml:"css"`      // CSS to apply
+  Children []*Style          `yaml:"children"` // Child rules, processed only if this one matches
 }
 
 func (s *Style) Apply(n *html.Node) error {
-	for k, v := range s.Css {
-		s.ApplyCSS(n, k, v)
-	}
-	return nil
+  for k, v := range s.Css {
+    s.ApplyCSS(n, k, v)
+  }
+  return nil
 }
 
 func (s *Style) ApplyCSS(n *html.Node, k, v string) {
-	key := "css-" + k
+  key := "css-" + k
 
-	// Overwrite any existing entry
-	for i, a := range n.Attr {
-		if a.Key == key {
-			n.Attr[i].Val = v
-			return
-		}
-	}
+  // Overwrite any existing entry
+  for i, a := range n.Attr {
+    if a.Key == key {
+      n.Attr[i].Val = v
+      return
+    }
+  }
 
-	n.Attr = append(n.Attr, html.Attribute{Key: key, Val: v})
+  n.Attr = append(n.Attr, html.Attribute{Key: key, Val: v})
 }
 
 func Parse(s []*Style) error {
-	for _, ve := range s {
-		r, err := ParseRule(ve.RuleSrc)
-		if err == nil {
-			ve.Rule = r
-			if len(ve.Children) > 0 {
-				err = Parse(ve.Children)
-			}
-		}
-		if err != nil {
-			return err
-		}
-	}
+  for _, ve := range s {
+    r, err := ParseRule(ve.RuleSrc)
+    if err == nil {
+      ve.Rule = r
+      if len(ve.Children) > 0 {
+        err = Parse(ve.Children)
+      }
+    }
+    if err != nil {
+      return err
+    }
+  }
 
-	return nil
+  return nil
 }
 
 func Write(w io.Writer, s []*Style) {
-	for _, ve := range s {
-		ve.Write(w)
-	}
+  for _, ve := range s {
+    ve.Write(w)
+  }
 }
 
 func (s *Style) Write(w io.Writer) {
-	s.Rule.Write(w)
+  s.Rule.Write(w)
 }
 
 func (c *Css) Start() error {
-	log.Println("Loading LaTeX CSS stylesheets")
-	err := Parse(c.Styles.Styles)
-	if err != nil {
-		return err
-	}
+  log.Println("Loading LaTeX CSS stylesheets")
+  err := Parse(c.Styles.Styles)
+  if err != nil {
+    return err
+  }
 
-	Write(os.Stdout, c.Styles.Styles)
+  Write(os.Stdout, c.Styles.Styles)
 
-	return io.EOF
+  return nil //io.EOF
 }
--- tools/latex/util/writer.go
+++ tools/latex/util/writer.go
@@ -1,95 +1,119 @@
 package util
 
 import (
-	"fmt"
-	"io"
+  "fmt"
+  "io"
 )
 
 // Writer assists in creating a LaTeX document
 type Writer struct {
-	w      io.WriteCloser // Underlying Writer
-	parent *Writer        // Parent when nesting
-	child  *Writer        // Child when nesting
-	class  string         // document in begin{document} etc. Used with end
+  w      io.WriteCloser // Underlying Writer
+  parent *Writer        // Parent when nesting
+  child  *Writer        // Child when nesting
+  class  string         // document in begin{document} etc. Used with end
+  blocks []string       // Slice of blocks to close
 }
 
 const (
-	Command1 = "\\%s{%s}\n"
-	Command2 = "\\%s{"
-	Command3 = "}\n"
+  Command1 = "\\%s{%s}\n"
+  Command2 = "\\%s{"
+  Command3 = "}\n"
 )
 
 func NewWriter(w io.WriteCloser) *Writer {
-	return &Writer{w: w}
+  return &Writer{w: w}
 }
 
 // Write implements io.Writer
 func (w *Writer) Write(p []byte) (n int, err error) {
-	return w.w.Write(p)
+  return w.w.Write(p)
 }
 
 // Close implements io.WriteCloser
 func (w *Writer) Close() error {
-	cw := w
+  cw := w
 
-	// Find youngest generation
-	for cw.child != nil {
-		cw = cw.child
-	}
+  // Find youngest generation
+  for cw.child != nil {
+    cw = cw.child
+  }
 
-	// Now end until we get to the root
-	for cw.parent != nil {
-		cw = cw.End()
-	}
+  // Now end until we get to the root
+  for cw.parent != nil {
+    cw = cw.End()
+  }
 
-	// Now close the writer
-	return cw.w.Close()
+  // Now close the writer
+  return cw.w.Close()
 }
 
 func (w *Writer) WriteString(f string, a ...interface{}) *Writer {
-	if len(a) == 0 {
-		_, _ = w.Write([]byte(f))
-	} else {
-		_, _ = w.Write([]byte(fmt.Sprintf(f, a...)))
-	}
-	return w
+  if len(a) == 0 {
+    _, _ = w.Write([]byte(f))
+  } else {
+    _, _ = w.Write([]byte(fmt.Sprintf(f, a...)))
+  }
+  return w
 }
 
 func (w *Writer) Begin(class string) *Writer {
-	if w.child != nil {
-		panic("begin when already with child")
-	}
-	w.WriteString(Command1, "begin", class)
+  if w.child != nil {
+    panic("begin when already with child")
+  }
+  w.WriteString(Command1, "begin", class)
 
-	cw := &Writer{
-		w:      w.w,
-		parent: w,
-		class:  class,
-	}
-	w.child = cw
-	return cw
+  cw := &Writer{
+    w:      w.w,
+    parent: w,
+    class:  class,
+  }
+  w.child = cw
+  return cw
 }
 
 func (w *Writer) End() *Writer {
-	if w.parent != nil {
-		if w.parent.child == nil {
-			panic("End from orphan")
-		}
-		_ = w.WriteString(Command1, "end", w.class)
-		w.parent.child = nil
-		return w.parent
-	}
-	return w
+  if w.parent != nil {
+    if w.parent.child == nil {
+      panic("End from orphan")
+    }
+    _ = w.WriteString(Command1, "end", w.class)
+    w.parent.child = nil
+    return w.parent
+  }
+  return w
 }
 
 func (w *Writer) Comment(f string, a ...interface{}) *Writer {
-	_ = w.WriteString("%% "+f+"\n", a...)
-	return w
+  _ = w.WriteString("%% "+f+"\n", a...)
+  return w
 }
 
 func (w *Writer) UsePackage(p ...string) *Writer {
-	for _, s := range p {
-		_ = w.WriteString(Command1, "usepackage", s)
-	}
-	return w
+  for _, s := range p {
+    _ = w.WriteString(Command1, "usepackage", s)
+  }
+  return w
+}
+
+func (w *Writer) Block(start, end string) *Writer {
+  w.WriteString(start)
+  w.blocks = append(w.blocks, end)
+  return w
+}
+
+func (w *Writer) EndAllBlocks() *Writer {
+  for len(w.blocks) > 0 {
+    w.EndBlock()
+  }
+  return w
+}
+
+func (w *Writer) EndBlock() *Writer {
+  l := len(w.blocks)
+  if l > 0 {
+    n := w.blocks[l-1]
+    w.blocks = w.blocks[:l-1]
+    w.WriteString(n)
+  }
+  return w
 }
